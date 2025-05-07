de851f65caac3d5f29552797fb75eb22153705ea
afraa78 <afraamoh178@gmail.com>
2025-05-07 22:57:40 +0300

    new

---
+++ j.java
@@ -0,0 +1,7 @@
+public class j {
+    public static void main(String[] args) {
+        System.out.println("Ids 20230709 20230721 20230750");
+        System.out.println("20230750");
+        System.out.println("20230709");
+    }
+}
